5658f55d890d4c0418b8be79d1cb4d15db33c8d3
gg117gg <100064038+gg117gg@users.noreply.github.com>
2026-05-06 11:00:35 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+// Smart View Player - Service Worker (오프라인 지원)
+const CACHE = "svp-v1";
+const ASSETS = [
+  "./",
+  "./SmartViewPlayer.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k!==CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return res;
+    }).catch(() => caches.match("./SmartViewPlayer.html")))
+  );
+});
